f2c3e0c5382fa0072192503b59f77bda23d09a5c
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2009-10-28 14:17:19 +0000

    πρόσθήκη δεύτερης γεωμετρίας
    --HG--
    extra : convert_revision : svn%3A38dfebb3-6f4a-b549-8164-1805876cbd73/trunk%405

--- vertex arrays/main.cpp
+++ vertex arrays/main.cpp
@@ -18,6 +18,7 @@ Color red = {1.0,0.0,0.0,1.0};
 Color yellow = {1.0,1.0,0.0,1.0};
 Color quarterOrange = {1.0,0.75,0.0,0.25};
 
+
 GLfloat vertices1[][2] = {
 	{-1.0,1.0}, {0.0,1.0}, {1.0,1.0},
 	{-1.0,0.0}, {0.0,0.0}, {1.0,0.0},
@@ -41,6 +42,28 @@ GLuint indices1[] = {
 	4,7,8
 };
 	
+GLfloat vertices2[][2] = {
+	{-0.50,0.50}, {0.0,2.0}, {0.50,0.50},
+	{-2.0,0.0}, {0.0,0.0}, {2.0,0.0},
+	{-0.50,-0.50}, {0.0,-2.0}, {0.50,-0.50}
+};
+
+Color colors2[] = {
+	quarterOrange,yellow,quarterOrange,
+	yellow,red,yellow,
+	quarterOrange,yellow,quarterOrange,
+};
+
+GLuint indices2[] = {
+	0,4,1,
+	1,4,2,
+	0,3,4,
+	2,4,5,
+	3,6,4,
+	4,8,5,
+	4,6,7,
+	4,7,8
+};
 
 
 
@@ -51,11 +74,34 @@ void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 
+	// transorm 1st shape
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(350,350,0);
+	glScalef(75,75,75);
+	glRotatef(45,0,0,1);
+
+	// draw 1st shape
 	glBegin(GL_TRIANGLES);
 		for(int c = 0 ; c < elementsOf(indices1) ; ++c)
 		{
 			glColor4fv((GLfloat*)&colors1[indices1[c]]);
 			glVertex2fv(&vertices1[indices1[c]][0]);
+		} // end for
+	glEnd();
+
+	// transorm 2nd shape
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(160,250,0);
+	glScalef(75,75,75);
+
+	// draw 2nd shape
+	glBegin(GL_TRIANGLES);
+		for(int c = 0 ; c < elementsOf(indices2) ; ++c)
+		{
+			glColor4fv((GLfloat*)&colors2[indices2[c]]);
+			glVertex2fv(&vertices2[indices2[c]][0]);
 		} // end for
 	glEnd();
 
@@ -78,7 +124,7 @@ void reshape (int w, int h)
 	glViewport(0, 0, (GLsizei) w, (GLsizei) h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D((GLdouble) -2, (GLdouble) 2, (GLdouble) -2, (GLdouble) 2);
+	gluOrtho2D(0, (GLdouble) w, 0, (GLdouble) h);
 } // end function reshape
 
 
@@ -87,8 +133,8 @@ int main(int argc, char **argv)
 	// glut initialization
 	glutInit(&argc,argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
-	glutInitWindowSize(320,320);
-	glutInitWindowPosition(800,160);
+	glutInitWindowSize(500,500);
+	glutInitWindowPosition(700,160);
 	glutCreateWindow("Creating Geometry");
 
 	// glew initialization
@@ -98,6 +144,9 @@ int main(int argc, char **argv)
 	glPolygonMode(GL_FRONT,GL_FILL);
 	glPolygonMode(GL_BACK,GL_LINE);
 	glShadeModel(GL_SMOOTH);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_BLEND);
+	glEnable(GL_LINE_SMOOTH);
 
 	// event handling initialization
 	glutDisplayFunc(display);
